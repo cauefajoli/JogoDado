c51e5b76044228b007e334e130ae234db2025346
Fajoli <Fajoli@FAJOLI-PC>
2017-02-20 22:14:54 -0300

    Add project files.

---
+++ JogoDado/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace JogoDado
+{
+    partial class frmJogoDado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblJogada1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbLance1Jogador1 = new System.Windows.Forms.TextBox();
+            this.txtbLance2Jogador1 = new System.Windows.Forms.TextBox();
+            this.txtbLance1Jogador2 = new System.Windows.Forms.TextBox();
+            this.txtbLance2Jogador2 = new System.Windows.Forms.TextBox();
+            this.txtbLance3Jogador1 = new System.Windows.Forms.TextBox();
+            this.txtbLance3Jogador2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnJogador1 = new System.Windows.Forms.Button();
+            this.btnJogador2 = new System.Windows.Forms.Button();
+            this.imgDado = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.imgDado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblJogada1
+            // 
+            this.lblJogada1.AutoSize = true;
+            this.lblJogada1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJogada1.Location = new System.Drawing.Point(12, 60);
+            this.lblJogada1.Name = "lblJogada1";
+            this.lblJogada1.Size = new System.Drawing.Size(60, 18);
+            this.lblJogada1.TabIndex = 1;
+            this.lblJogada1.Text = "Lance 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jogador 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(185, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Jogador 2";
+            // 
+            // txtbLance1Jogador1
+            // 
+            this.txtbLance1Jogador1.Enabled = false;
+            this.txtbLance1Jogador1.Location = new System.Drawing.Point(107, 61);
+            this.txtbLance1Jogador1.Name = "txtbLance1Jogador1";
+            this.txtbLance1Jogador1.Size = new System.Drawing.Size(72, 20);
+            this.txtbLance1Jogador1.TabIndex = 4;
+            this.txtbLance1Jogador1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtbLance2Jogador1
+            // 
+            this.txtbLance2Jogador1.Enabled = false;
+            this.txtbLance2Jogador1.Location = new System.Drawing.Point(107, 104);
+            this.txtbLance2Jogador1.Name = "txtbLance2Jogador1";
+            this.txtbLance2Jogador1.Size = new System.Drawing.Size(72, 20);
+            this.txtbLance2Jogador1.TabIndex = 5;
+            this.txtbLance2Jogador1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtbLance1Jogador2
+            // 
+            this.txtbLance1Jogador2.Enabled = false;
+            this.txtbLance1Jogador2.Location = new System.Drawing.Point(188, 61);
+            this.txtbLance1Jogador2.Name = "txtbLance1Jogador2";
+            this.txtbLance1Jogador2.Size = new System.Drawing.Size(72, 20);
+            this.txtbLance1Jogador2.TabIndex = 6;
+            this.txtbLance1Jogador2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtbLance2Jogador2
+            // 
+            this.txtbLance2Jogador2.Enabled = false;
+            this.txtbLance2Jogador2.Location = new System.Drawing.Point(188, 104);
+            this.txtbLance2Jogador2.Name = "txtbLance2Jogador2";
+            this.txtbLance2Jogador2.Size = new System.Drawing.Size(72, 20);
+            this.txtbLance2Jogador2.TabIndex = 7;
+            this.txtbLance2Jogador2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtbLance3Jogador1
+            // 
+            this.txtbLance3Jogador1.Enabled = false;
+            this.txtbLance3Jogador1.Location = new System.Drawing.Point(107, 144);
+            this.txtbLance3Jogador1.Name = "txtbLance3Jogador1";
+            this.txtbLance3Jogador1.Size = new System.Drawing.Size(72, 20);
+            this.txtbLance3Jogador1.TabIndex = 8;
+            this.txtbLance3Jogador1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtbLance3Jogador2
+            // 
+            this.txtbLance3Jogador2.Enabled = false;
+            this.txtbLance3Jogador2.Location = new System.Drawing.Point(188, 144);
+            this.txtbLance3Jogador2.Name = "txtbLance3Jogador2";
+            this.txtbLance3Jogador2.Size = new System.Drawing.Size(72, 20);
+            this.txtbLance3Jogador2.TabIndex = 9;
+            this.txtbLance3Jogador2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lance 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lance 3";
+            // 
+            // btnJogador1
+            // 
+            this.btnJogador1.Location = new System.Drawing.Point(12, 211);
+            this.btnJogador1.Name = "btnJogador1";
+            this.btnJogador1.Size = new System.Drawing.Size(126, 23);
+            this.btnJogador1.TabIndex = 10;
+            this.btnJogador1.Tag = "1";
+            this.btnJogador1.Text = "Jogador 1";
+            this.btnJogador1.UseVisualStyleBackColor = true;
+            this.btnJogador1.Click += new System.EventHandler(this.btnJogador1_Click);
+            // 
+            // btnJogador2
+            // 
+            this.btnJogador2.Enabled = false;
+            this.btnJogador2.Location = new System.Drawing.Point(144, 211);
+            this.btnJogador2.Name = "btnJogador2";
+            this.btnJogador2.Size = new System.Drawing.Size(126, 23);
+            this.btnJogador2.TabIndex = 11;
+            this.btnJogador2.Tag = "1";
+            this.btnJogador2.Text = "Jogador 2";
+            this.btnJogador2.UseVisualStyleBackColor = true;
+            this.btnJogador2.Click += new System.EventHandler(this.btnJogador2_Click);
+            // 
+            // imgDado
+            // 
+            this.imgDado.Image = global::JogoDado.Properties.Resources.dado;
+            this.imgDado.Location = new System.Drawing.Point(287, 22);
+            this.imgDado.Name = "imgDado";
+            this.imgDado.Size = new System.Drawing.Size(200, 196);
+            this.imgDado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgDado.TabIndex = 0;
+            this.imgDado.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 200;
+            // 
+            // frmJogoDado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 261);
+            this.Controls.Add(this.btnJogador2);
+            this.Controls.Add(this.btnJogador1);
+            this.Controls.Add(this.txtbLance3Jogador2);
+            this.Controls.Add(this.txtbLance3Jogador1);
+            this.Controls.Add(this.txtbLance2Jogador2);
+            this.Controls.Add(this.txtbLance1Jogador2);
+            this.Controls.Add(this.txtbLance2Jogador1);
+            this.Controls.Add(this.txtbLance1Jogador1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblJogada1);
+            this.Controls.Add(this.imgDado);
+            this.Name = "frmJogoDado";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jodo de Dados";
+            ((System.ComponentModel.ISupportInitialize)(this.imgDado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgDado;
+        private System.Windows.Forms.Label lblJogada1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbLance1Jogador1;
+        private System.Windows.Forms.TextBox txtbLance2Jogador1;
+        private System.Windows.Forms.TextBox txtbLance1Jogador2;
+        private System.Windows.Forms.TextBox txtbLance2Jogador2;
+        private System.Windows.Forms.TextBox txtbLance3Jogador1;
+        private System.Windows.Forms.TextBox txtbLance3Jogador2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnJogador1;
+        private System.Windows.Forms.Button btnJogador2;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ JogoDado/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDado
+{
+    public partial class frmJogoDado : Form
+    {
+        private int j1;
+        private int j2;
+
+        public frmJogoDado()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJogador1_Click(object sender, EventArgs e)
+        {
+            String jogada = btnJogador1.Tag.ToString();
+            Random rnd = new Random();
+            int valor = rnd.Next(1, 7);
+            selecionaDado(valor);
+            switch (jogada)
+            {
+                case "1":
+                    txtbLance1Jogador1.Text = valor.ToString();
+                    btnJogador1.Tag = "2";
+                    btnJogador1.Enabled = false;
+                    btnJogador2.Enabled = true;
+                    break;
+                case "2":
+                    txtbLance2Jogador1.Text = valor.ToString();
+                    btnJogador1.Tag = "3";
+                    btnJogador1.Enabled = false;
+                    btnJogador2.Enabled = true;
+                    break;
+                case "3":
+                    txtbLance3Jogador1.Text = valor.ToString();
+                    btnJogador1.Enabled = false;
+                    btnJogador2.Enabled = true;
+                    break;
+            }
+            
+        }
+
+        private void btnJogador2_Click(object sender, EventArgs e)
+        {
+            String jogada = btnJogador2.Tag.ToString();
+            Random rnd = new Random();
+            int valor = rnd.Next(1, 7);
+            selecionaDado(valor);
+            switch (jogada)
+            {
+                case "1":
+                    txtbLance1Jogador2.Text = valor.ToString();
+                    btnJogador2.Tag = "2";
+                    btnJogador2.Enabled = false;
+                    btnJogador1.Enabled = true;
+                    break;
+                case "2":
+                    txtbLance2Jogador2.Text = valor.ToString();
+                    btnJogador2.Tag = "3";
+                    btnJogador2.Enabled = false;
+                    btnJogador1.Enabled = true;
+                    verificaGanhador(jogada);
+                    break;
+                case "3":
+                    txtbLance3Jogador2.Text = valor.ToString();
+                    btnJogador2.Enabled = false;
+                    verificaGanhador(jogada);
+                    break;
+            }
+        }
+
+        private void verificaGanhador(string jogada)
+        {
+            switch (jogada)
+            {
+                case "2":
+                    if (Int32.Parse(txtbLance1Jogador1.Text) > Int32.Parse(txtbLance1Jogador2.Text))
+                    {
+                        j1 += 1;
+                    }
+                    else if (Int32.Parse(txtbLance1Jogador1.Text) < Int32.Parse(txtbLance1Jogador2.Text))
+                    {
+                        j2 += 1;
+                    }
+
+                    if (Int32.Parse(txtbLance2Jogador1.Text) > Int32.Parse(txtbLance2Jogador2.Text))
+                    {
+                        j1 += 1;
+                    }
+                    else if (Int32.Parse(txtbLance2Jogador1.Text) < Int32.Parse(txtbLance2Jogador2.Text))
+                    {
+                        j2 += 1;
+                    }
+
+                    if (j1 > j2 && j1 > 1)
+                    {
+                        MessageBox.Show("Jogador 1 é o vencedor!");
+                        Application.Exit();
+                    }
+                    else if (j2 > j1 && j2 > 1)
+                    {
+                        MessageBox.Show("Jogador 2 é o vencedor!");
+                        Application.Exit();
+                    }
+                    break;
+
+                case "3":
+                    if (Int32.Parse(txtbLance3Jogador1.Text) > Int32.Parse(txtbLance3Jogador2.Text))
+                    {
+                        j1 += 1;
+                    }
+                    else if (Int32.Parse(txtbLance3Jogador1.Text) < Int32.Parse(txtbLance3Jogador2.Text))
+                    {
+                        j2 += 1;
+                    }
+
+
+                    if (j1 > j2)
+                    {
+                        MessageBox.Show("Jogador 1 é o vencedor!");
+                        Application.Exit();
+                    }
+                    else if (j2 > j1)
+                    {
+                        MessageBox.Show("Jogador 2 é o vencedor!");
+                        Application.Exit();
+                    }
+                    else if (j2 == j1)
+                    {
+                        MessageBox.Show("Empate!");
+                        Application.Exit();
+                    }
+                    break;
+            }
+        }
+
+        private void selecionaDado(int valor)
+        {
+            switch (valor)
+            {
+                case 1:
+                    imgDado.Image = JogoDado.Properties.Resources._1;
+                    break;
+                case 2:
+                    imgDado.Image = JogoDado.Properties.Resources._2;
+                    break;
+                case 3:
+                    imgDado.Image = JogoDado.Properties.Resources._3;
+                    break;
+                case 4:
+                    imgDado.Image = JogoDado.Properties.Resources._4;
+                    break;
+                case 5:
+                    imgDado.Image = JogoDado.Properties.Resources._5;
+                    break;
+                case 6:
+                    imgDado.Image = JogoDado.Properties.Resources._6;
+                    break;
+            }
+        }
+    }
+}
